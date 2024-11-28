8147e63107dea6905444cc4dc4cceecac76e5832
Swatantra Pratap Singh <swatantra3577@gmail.com>
2024-11-28 20:39:44 +0530

    Update script.js
    Added on script file

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ btn.addEventListener("click", () =>{
                 speak("opening your project azad sir ...")
                 window.open("https://swatantra35.github.io/Amazon-Clone/","_blank");
                 break;
-             case message.includes("WHO IS MY BEST FRIENDS"):
+             case message.includes("WHO IS MY BEST FRIEND"):
                 speak("YOURS FRIENDS ARE DEEPANSHU, ADITYA, FAKIYA DARLING ...")
                 break;
             
